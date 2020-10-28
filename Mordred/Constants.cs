d8176d488f50cc07d488df038ed0e71662819678
Venomous <stefvandenbruel@outlook.com>
2020-10-28 23:33:20 +0100

    Increase game window size + world size

--- Mordred/Constants.cs
+++ Mordred/Constants.cs
@@ -4,8 +4,8 @@
     {
         public static class GameSettings
         {
-            public const int GameWindowWidth = 100;
-            public const int GameWindowHeight = 40;
+            public const int GameWindowWidth = 140;
+            public const int GameWindowHeight = 50;
             public const float TimePerTickInSeconds = 0.25f;
         }
 
